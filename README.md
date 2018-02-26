b14d747f8331d19492eab240d534d3e28f5cc2e8
Rudi Kershaw <rudi.kershaw@cognitran.com>
2018-02-26 18:22:02 +0000

    Rudi Kershaw - Update comments and readme

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
-# gitcloc
-Shell script for clearing/deleting all local branches that have been merged into another given branch
+# Clear merged in local branches
+The shell script `gitcloc` is for clearing/deleting all local branches that have been merged into another given branch.
+
+# Usage
+
+Below is the output of `gitcloc --help`
 
 ```
 usage: gitcloc branch [--help]
@@ -9,3 +13,12 @@ The branch must be provided as the first arguement even when calling with option
 optional arguements:
     --help                   Shows this help message
 ```
+
+To use `gitcloc` simply navigate to the git repository which you want to delete local merged branches. Lets say we want to remove all local branches that have been merged into a branch called `develop`. You might write the following:
+
+```bash
+$ cd /home/rudi/path/to/your/repository
+$ gitcloc develop
+```
+
+You will then be presented with the git command line editor of your choice, showing a new-line delimited list of the branches that will be removed. Remove or add any branches as appropriate. Then save your changes and exit the editor. The script will then use a soft delete on each branch (branches that have not been merged into a remote branch that they are tracking will fail and an error will be presented for each failing branch) and confirm all deleted branches.
--- gitcloc.sh
+++ gitcloc.sh
@@ -18,28 +18,31 @@ else
     TMP_BRANCHES_FILE=$(mktemp)
     trap "{ rm -f $TMP_BRANCHES_FILE; }" EXIT
 
-    # Redirect merged branches to tmp file and interactively edit file
+    # Store current branch and switch to target branch.
     CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
     if [ "$CURRENT_BRANCH" != "$1" ]; then
         git checkout $1
     fi
 
+    # Redirect merged branches to tmp file for interactive editing.
     git branch --merged > $TMP_BRANCHES_FILE
-    # Trim trailing and leading space, and empty lines, etc, before edit.
+    # Trim trailing/leading space, empty lines, and current branch before edit.
     sed -i 's/^ *//; s/ *$//; /^$/d; /^[*]/d' $TMP_BRANCHES_FILE
 
+    # Check that there are merged branches to be delete before continueing.
     LINE_COUNT=$(wc -l "$TMP_BRANCHES_FILE" | sed 's/ .*//')
     if [[ $LINE_COUNT -gt 0 ]]; then
         $(git var GIT_EDITOR) $TMP_BRANCHES_FILE
-        # Trim trailing and leading space, and empty lines, etc, after edit.
+        # Trim trailing/leading space, and empty lines after edit.
         sed -i 's/^ *//; s/ *$//; /^$/d' $TMP_BRANCHES_FILE
-        # Soft delete all branches left in the file and then remove tmp file.
+        # Soft delete all branches left in the file.
         set +e
         xargs git branch -d < $TMP_BRANCHES_FILE
     else
         echo "No branches found for clearing."
     fi
 
+    # Switch back to original branch if it still exists.
     set -e
     if [ "$CURRENT_BRANCH" != "$1" ]; then
         git checkout $CURRENT_BRANCH 2> /dev/null
